3daffe1c9c015f87ad56d191e675de6c29aedf9b
Kashif Ali <kashifali874@gmail.com>
2017-05-08 19:17:18 -0700

    pretty url

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  resources :pages, only: [:show]
   namespace :admin do
     resources :pages # /admin/pages
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/:id' => 'pages#show'
+  # resources :pages
+  # Page.where.not("slug", nil).all.each do |page|
+  #   get "/#{page.slug}", controller: "pages", action: "show", id: page.id
+  # end
 end
