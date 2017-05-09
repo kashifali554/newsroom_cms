118bd62b5023d40c10bc040213249ce93b401c19
Kashif Ali <kashifali874@gmail.com>
2017-05-08 20:15:48 -0700

    category added, db migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170509005052) do
+ActiveRecord::Schema.define(version: 20170509030832) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "pages", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
     t.string   "slug"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "category_id"
+    t.index ["category_id"], name: "index_pages_on_category_id", using: :btree
   end
 
+  add_foreign_key "pages", "categories"
 end
