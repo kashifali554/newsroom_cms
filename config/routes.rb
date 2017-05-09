1bb21ad300db7762779fe6df49a38c3474c2bf91
Kashif Ali <kashifali874@gmail.com>
2017-05-08 20:03:16 -0700

    loads of styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  resources :pages, only: [:show]
   namespace :admin do
     resources :pages # /admin/pages
   end
 
   get '/:id' => 'pages#show'
+
+  root 'admin/pages#index'
   # resources :pages
   # Page.where.not("slug", nil).all.each do |page|
   #   get "/#{page.slug}", controller: "pages", action: "show", id: page.id
